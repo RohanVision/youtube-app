6d6bb0931154297ef2d5113246008033dc2c1299
Rohan Amberkar <amberkar.rohan17@gmail.com>
2025-01-04 18:04:50 -0500

    Components Skeloton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Body from './components/Body';
+import Head from './components/Head';
 
 function App() {
   return (
     <div className="App">
       <h1 className='bg-red-500 text-3xl'>Youtube</h1>
+      <Head />
+      <Body />
     </div>
   );
 }
