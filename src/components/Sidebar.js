993f8e27e6d8da2160f0444fb904a984b83a9ac8
Rohan Amberkar <amberkar.rohan17@gmail.com>
2025-01-04 19:02:36 -0500

    Sidebar UI Done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,8 +2,32 @@ import React from 'react'
 
 const Sidebar = () => {
     return (
-        <div>
-            Sidebar
+        <div className='p-4 w-40 shadow-lg h-full'>
+            <ul className='pl-4 mb-6'>
+                <li>Home</li>
+                <li>Shorts</li>
+                <li>Subscription</li>
+            </ul>
+            <h2 className='font-bold'>You</h2>
+            <ul className='pl-4 mb-6'>
+                <li>History</li>
+                <li>Playlist</li>
+                <li>Your Videos</li>
+                <li>Watch Later</li>
+                <li>Liked Videos</li>
+            </ul>
+            <h2 className='font-bold'>Exploer</h2>
+            <ul className='pl-4'>
+                <li>Trending</li>
+                <li>Music</li>
+                <li>Movies & TV</li>
+                <li>Live</li>
+                <li>News</li>
+                <li>Learning</li>
+                <li>Fashion & Beauty</li>
+                <li>Podcasts</li>
+                <li>Playables</li>
+            </ul>
         </div>
     )
 }
