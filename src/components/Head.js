0c72ad98f1501074f0b5c43573b809ed88554011
Rohan Amberkar <amberkar.rohan17@gmail.com>
2025-01-06 17:41:38 -0500

    Suggestion on Search with Debouncing Functionality

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,23 +1,57 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { toggleMenu } from '../utils/appSlice';
+import { YOUTUBE_SEARCH_API } from '../utils/constants';
 
 const Head = () => {
-    const dispatch = useDispatch();
+    const [searchQuery, setSearchQuery] = useState("");
+    const [suggestion, setSuggestion] = useState([]);
+    const [showSuggestion, setShowSuggestion] = useState(false);
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            return getSearchSuggestion();
+        }, 200);
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [searchQuery]);
+
+    const getSearchSuggestion = async () => {
+        console.log("API CALL" + searchQuery);
+        const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+        const json = await data.json();
+        // console.log(json);
+        setSuggestion(json[1])
+    }
+
+    const dispatch = useDispatch();
     const toggleMenuHandler = () => {
         dispatch(toggleMenu());
     }
-
     return (
         <div className='flex justify-between p-4 shadow-sm'>
             <div className='flex items-start'>
                 <img onClick={() => toggleMenuHandler()} className='h-8 cursor-pointer' src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/640px-Hamburger_icon.svg.png" alt="menu" />
                 <img className='w-48 h-8' src="https://www.logo.wine/a/logo/YouTube/YouTube-Logo.wine.svg" alt="Youtube-Logo" />
             </div>
-            <div className='search-bar flex items-center'>
-                <input className='border border-gray-300 w-96 h-8 p-4 outline-none rounded-l-full' type="text" />
-                <button className='bg-gray-200 py-2 px-4 border rounded-r-full'><img className='h-4' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Magnifying_glass_icon.svg/800px-Magnifying_glass_icon.svg.png" alt="" /></button>
+            <div className='search-bar flex items-center flex-col justify-start '>
+                <div>
+                    <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onFocus={() => setShowSuggestion(true)} onBlur={() => setShowSuggestion(false)} className='border border-gray-300 w-96 h-8 p-4 outline-none rounded-l-full' type="text" />
+                    <button className='bg-gray-200 py-2 px-4 border rounded-r-full'><img className='h-4' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Magnifying_glass_icon.svg/800px-Magnifying_glass_icon.svg.png" alt="search-icon" />
+                    </button>
+                </div>
+                {showSuggestion && <div className='w-full relative'>
+                    <ul className='fixed bg-gray-200 w-96 rounded-sm shadow-md border-gray-100'>
+                        {
+                            suggestion.map((suggest) => {
+                                return (
+                                    <li key={suggest} className='hover:bg-gray-500 hover:text-white    px-2 py-2'><img className='h-4 pr-2 inline' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Magnifying_glass_icon.svg/800px-Magnifying_glass_icon.svg.png" alt="search-icon" />{suggest}</li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>}
             </div>
             <div className='user'>
                 <img className='h-8' src="https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png" alt="user-icon" />
---
+++ src/utils/SearchSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const SearchSlice = createSlice({
+    name: "search",
+    initialState: {
+
+    }
+})
