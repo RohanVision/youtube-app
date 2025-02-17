72ca7deee65dd77d05900ff033dd953d8d83558a
Rohan Amberkar <amberkar.rohan17@gmail.com>
2025-02-16 20:42:24 -0500

    CORS issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,14 @@
 export const GOOGLE_API_KEY = "AIzaSyD0oikU5dO-bEBTZmnUpE35wOCIi6b5e04";
-export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&regionCode=CA&key=" + GOOGLE_API_KEY;
 
-export const VIDEO_DETAILS = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=";
+export const YOUTUBE_VIDEO_API = "https://proxy.cors.sh/https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&regionCode=CA&key=" + GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const VIDEO_DETAILS = "https://proxy.cors.sh/https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=";
 
-export const YOUTUBE_MY_VIDEO = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=6&regionCode=CA&key=' + GOOGLE_API_KEY;
+export const YOUTUBE_SEARCH_API = "https://proxy.cors.sh/http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
-export const SERACH_VIDEO = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=';
+export const YOUTUBE_MY_VIDEO = 'https://proxy.cors.sh/https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=6&regionCode=CA&key=' + GOOGLE_API_KEY;
+
+export const SERACH_VIDEO = 'https://proxy.cors.sh/https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=';
 
 export const OFFSET_LIVE_CHAT = 20;
 
